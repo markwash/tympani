f215c2fae43ca9ae74d8fd1db887a76b10050977
Mark Washenberger <mark.washenberger@rackspace.com>
2010-12-15 18:54:32 -0500

    get displace_sound.cc working

--- src/displaced_sound.cc
+++ src/displaced_sound.cc
@@ -2,6 +2,21 @@
 
 #include <stdlib.h>
 
+int sound(int time, int period) {
+  int rel = time % period;
+  int level;
+  if (rel < period/2)
+    level = 4 * 0x6000 * rel / period - 0x6000;
+  else
+    level = 3 * 0x6000 - 4 * 0x6000 * rel / period;
+  if (level > 0x4000)
+    return 0x4000;
+  else if (level < -0x4000)
+    return -0x4000;
+  else
+    return level;
+}
+
 int main(int argc, char **argv) {
 
   const char *path = argv[1];
@@ -9,16 +24,18 @@ int main(int argc, char **argv) {
   int offset = atoi(argv[3]);
 
   SndfileHandle file = SndfileHandle(path, SFM_WRITE, 0x10002, 2, 44100);
+  int period = 44100 / 260;
 
-  int increase = 0x10000 / 260;
-  short left_level = 0;
-  short right_level = (left_level + offset * increase) % 0x10000;
-  int frame[2];
-  for (int i = 0; i < 44100 * time; i++) {
-    frame[0] = left_level;
-    frame[1] = right_level;
-    left_level = (left_level + increase) % 0x10000;
-    right_level = (right_level + increase) % 0x10000;
-    file.writef(frame, 1);
-  }
+  short buf[2 * 4096];
+  int count = 0;
+  while (count < 44100 * time) {
+    for (int i = 0; i < 4096; i++) {
+      buf[2 * i] = sound(count, period);
+      buf[2 * i + 1] = sound(count + offset, period);
+      //buf[2 * i] = (count / period) % 2 ? 0x4000 : -0x4000;
+      //buf[2 * i + 1] = ((count + offset) / period) % 2 ? 0x4000 : -0x4000;
+      count++;
+    }
+    file.writef(buf, 4096);
+  } 
 }
